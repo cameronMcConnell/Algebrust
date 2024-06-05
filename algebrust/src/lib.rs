2694ffa090c03f7cd1bbdb9852eaf2c35cb46908
Cameron McConnell <cameron@angry-dogman.com>
2024-06-05 11:59:30 -0500

    Added basic functionality for the matrix struct.

--- algebrust/src/lib.rs
+++ algebrust/src/lib.rs
@@ -1,22 +1,27 @@
-use std::iter::Sum;
-
 use rand::{distributions::Uniform, Rng};
 
 pub struct AlgebrustVector {
-    pub vector: Vec<f32>
+    vector: Vec<f64>
 }
 
+pub struct AlgebrustMatrix {
+    matrix: Vec<Vec<f64>>
+}
+
+
 impl AlgebrustVector {
-    pub fn new(vector: &[f32]) -> Self {
+    pub fn new(vector: &[f64]) -> Self {
         AlgebrustVector { 
             vector: vector.to_vec()
         }
     }
 
-    pub fn new_rand(length: usize, low: f32, high: f32) -> Self {
+    pub fn new_rand(length: usize, low: f64, high: f64) -> Self {
         let mut rng = rand::thread_rng();
         let range = Uniform::new(low, high);
-        let vector: Vec<f32> = (0..length).map(|_| rng.sample(&range)).collect();
+        let vector: Vec<f64> = (0..length)
+            .map(|_| rng.sample(&range))
+            .collect();
         AlgebrustVector { vector }
     }
 
@@ -29,40 +34,106 @@ impl AlgebrustVector {
         self.vector.len()
     }
 
+    pub fn vector_ref(&self) -> &Vec<f64> {
+        &self.vector
+    }
+
     pub fn addition(&self, other: &AlgebrustVector) -> AlgebrustVector {
         assert_eq!(self.len(), other.len());
-        let vector: Vec<f32> = self.vector.iter().zip(&other.vector).map(|(a, b)| a + b).collect();
+        let vector: Vec<f64> = self.vector
+            .iter()
+            .zip(&other.vector)
+            .map(|(a, b)| a + b)
+            .collect();
         AlgebrustVector { vector }
     }
 
     pub fn substraction(&self, other: &AlgebrustVector) -> AlgebrustVector {
         assert_eq!(self.len(), other.len());
-        let vector: Vec<f32> = self.vector.iter().zip(&other.vector).map(|(a, b)| a - b).collect();
+        let vector: Vec<f64> = self.vector
+            .iter()
+            .zip(&other.vector)
+            .map(|(a, b)| a - b)
+            .collect();
         AlgebrustVector { vector }
     }
 
-    pub fn dot_product(&self, other: &AlgebrustVector) -> f32 {
+    pub fn dot_product(&self, other: &AlgebrustVector) -> f64 {
         assert_eq!(self.len(), other.len());
-        self.vector.iter().zip(&other.vector).map(|(a, b)| a * b).sum()
+        self.vector
+            .iter()
+            .zip(&other.vector)
+            .map(|(a, b)| a * b)
+            .sum()
     }
 
-    pub fn scalar_multiplication(&self, scaler: f32) -> AlgebrustVector {
-        let vector: Vec<f32> = self.vector.iter().map(|a| a * scaler).collect();
+    pub fn scalar_multiplication(&self, scaler: f64) -> AlgebrustVector {
+        let vector: Vec<f64> = self.vector
+            .iter()
+            .map(|a| a * scaler)
+            .collect();
         AlgebrustVector { vector }
     }
 
-    pub fn magnitude(&self) -> f32 {
-        let squared_sum: f32 = self.vector.iter().map(|a| a * a).sum();
+    pub fn magnitude(&self) -> f64 {
+        let squared_sum: f64 = self.vector
+            .iter()
+            .map(|a| a * a)
+            .sum();
         squared_sum.sqrt()
     }
 
     pub fn normalization(&self) -> AlgebrustVector {
         let magnitude = self.magnitude();
-        let vector: Vec<f32> = self.vector.iter().map(|a| a / magnitude).collect();
+        let vector: Vec<f64> = self.vector
+            .iter()
+            .map(|a| a / magnitude)
+            .collect();
         AlgebrustVector { vector }
     }
 
-    pub fn cross_product(&self) -> AlgebrustVector {
-        
+    pub fn cross_product(&self, other: &AlgebrustVector) -> AlgebrustVector {
+        assert_eq!((self.len(), other.len()), (3, 3));
+        AlgebrustVector { 
+            vector: vec![
+                (self.vector[1] * other.vector[2]) - (self.vector[2] * other.vector[1]),
+                (self.vector[2] * other.vector[0]) - (self.vector[0] * other.vector[2]),
+                (self.vector[0] * other.vector[1]) - (self.vector[1] * other.vector[0])
+            ] 
+        }
+    }
+}
+
+impl AlgebrustMatrix {
+    pub fn new(matrix: &[&[f64]]) -> Self {
+        let matrix: Vec<Vec<f64>> = matrix
+            .iter()
+            .map(|vector| vector.to_vec())
+            .collect();
+        AlgebrustMatrix { matrix }
+    }
+
+    pub fn new_rand(size: (usize, usize), low: f64, high: f64) -> Self {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(low, high);
+        let matrix: Vec<Vec<f64>> = (0..size.0)
+            .map(|_| (0..size.1).map(|_| rng.sample(&range)).collect())
+            .collect();
+        AlgebrustMatrix { matrix }
+    }
+
+    pub fn new_zeros(size: (usize, usize)) -> Self {
+        let matrix: Vec<Vec<f64>> = (0..size.0)
+            .map(|_| vec![0.0; size.1])
+            .collect();
+        AlgebrustMatrix { matrix }
+    }
+
+    pub fn size(&self) -> (usize, usize) {
+        (self.matrix.len(), self.matrix[0].len())
+    }
+
+    pub fn matrix_ref(&self) -> &Vec<Vec<f64>> {
+        &self.matrix
     }
 }
