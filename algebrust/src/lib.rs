788273d4137b69ef8461076d3d23894c48119c0e
Cameron McConnell <cameron@angry-dogman.com>
2024-06-06 12:08:25 -0500

    Finished defining and coding functions for the matrix struct.
    Now testing is the only part that is required.

--- algebrust/src/lib.rs
+++ algebrust/src/lib.rs
@@ -129,12 +129,59 @@ impl AlgebrustMatrix {
         AlgebrustMatrix { matrix }
     }
 
+    pub fn new_identity(&self, n: usize) -> Self {
+        let matrix = Self::get_identity_matrix(n);
+        AlgebrustMatrix { matrix }
+    }
+
     pub fn size(&self) -> (usize, usize) {
         (self.matrix.len(), self.matrix[0].len())
     }
 
     pub fn matrix_ref(&self) -> &Vec<Vec<f64>> {
         &self.matrix
+    }
+
+    fn get_column(&self, column_index: usize, other: &AlgebrustMatrix) -> Vec<f64> {
+        other.matrix.iter().map(|row| row[column_index]).collect()
+    }
+
+    fn get_identity_matrix(n: usize) -> Vec<Vec<f64>> {
+        let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; n]; n];
+        for i in 0..n { matrix[i][i] = 1.0; }
+        matrix
+    }
+
+    fn get_diagonal_product(&self, matrix: &Vec<Vec<f64>>, n: usize) -> f64 {
+        (0..n)
+            .map(|i| matrix[i][i])
+            .product()
+    }
+
+    fn forward_substitution(&self, l: &Vec<Vec<f64>>, b: &Vec<f64>) -> Vec<f64> {
+        let n = l.len();
+        let mut x = vec![0.0; n];
+        for i in 0..n {
+            let summation: f64 = (0..i)
+                .map(|j| l[i][j] * x[j])
+                .sum();
+            x[i] = (b[i] - summation) / l[i][i];
+        }
+        
+        x
+    }
+
+    fn backward_substitution(&self, u: &Vec<Vec<f64>>, x: &Vec<f64>) -> Vec<f64> {
+        let n = u.len();
+        let mut y = vec![0.0; n];
+        for i in (0..n).rev() {
+            let summation: f64 = ((i + 1)..n)
+                .map(|j| u[i][j] * x[j])
+                .sum();
+            y[i] = (x[i] - summation) / u[i][i];
+        }
+        
+        y
     }
 
     pub fn addition(&self, other: &AlgebrustMatrix) -> AlgebrustMatrix {
@@ -166,10 +213,12 @@ impl AlgebrustMatrix {
     }
 
     pub fn multiplication(&self, other: &AlgebrustMatrix) -> AlgebrustMatrix {
-        assert_eq!(self.size().1, other.size().0);
-        let mut matrix = vec![vec![0.0; other.size().1]; self.size().0];
-        for i in 0..self.size().0 {
-            for j in 0..other.size().1 {
+        let (n, o) = self.size();
+        let (p, m) = other.size();
+        assert_eq!(o, p);
+        let mut matrix = vec![vec![0.0; m]; n];
+        for i in 0..n {
+            for j in 0..m {
                 matrix[i][j] = self.get_column(j, other)
                     .iter()
                     .zip(&self.matrix[i])
@@ -178,10 +227,6 @@ impl AlgebrustMatrix {
             }
         }
         AlgebrustMatrix { matrix }
-    }
-
-    fn get_column(&self, column_index: usize, other: &AlgebrustMatrix) -> Vec<f64> {
-        other.matrix.iter().map(|row| row[column_index]).collect()
     }
 
     pub fn scalar_multiplication(&self, scaler: f64) -> AlgebrustMatrix {
@@ -193,5 +238,58 @@ impl AlgebrustMatrix {
                 .collect())
             .collect();
         AlgebrustMatrix { matrix }
+    }
+
+    pub fn transpose(&self) -> AlgebrustMatrix {
+        let (n, m) = self.size();
+        let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; n]; m];
+        for i in 0..n {
+            for j in 0..m {
+                matrix[j][i] = self.matrix[i][j];
+            }
+        }
+        AlgebrustMatrix { matrix }
+    }
+
+    pub fn lu_decomposition(&self) -> (AlgebrustMatrix, AlgebrustMatrix) {
+        let (n, m) = self.size();
+        assert_eq!(n, m);
+        let mut l: Vec<Vec<f64>> = Self::get_identity_matrix(n);
+        let mut u: Vec<Vec<f64>> = self.matrix.clone();
+        for j in 0..n {
+            for i in j+1..n {
+                l[i][j] = u[i][j] / u[j][j];
+                for k in j..n {
+                    u[i][k] = u[i][k] - l[i][j] * u[j][k];
+                }
+            }
+        }
+        (AlgebrustMatrix{matrix: l}, AlgebrustMatrix{matrix: u})
+    }
+
+    pub fn determinant(&self) -> f64 {
+        let (n, m) = self.size();
+        assert_eq!(n, m);
+        let (l, u) = self.lu_decomposition();
+        let determinant_l: f64 = self.get_diagonal_product(l.matrix_ref(), n);
+        let determinant_u: f64 = self.get_diagonal_product(u.matrix_ref(), n);
+        determinant_l * determinant_u
+    }
+
+    pub fn inverse(&self) -> AlgebrustMatrix {
+        let (n, m) = self.size();
+        assert_eq!(n, m);
+        let (l, u) = self.lu_decomposition();
+        let identity_matrix = AlgebrustMatrix { matrix: Self::get_identity_matrix(n) };
+        let mut inverse_matrix = vec![vec![0.0; n]; n];
+        for i in 0..n {
+            let e_i = self.get_column(i, &identity_matrix);
+            let x_i = self.forward_substitution(l.matrix_ref(), &e_i);
+            let y_i = self.backward_substitution(u.matrix_ref(), &x_i);
+            for j in 0..n {
+                inverse_matrix[j][i] = y_i[j];
+            }
+        }
+        AlgebrustMatrix { matrix: inverse_matrix }
     }
 }
